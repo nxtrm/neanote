dae38e60e68d7c3fd19c087c55d7ab8f808e13ba
nxtrm <nextrem_p@proton.me>
2024-06-09 19:12:32 +0300

    updated component library imports

--- neanote/components/Layout/Layout.tsx
+++ neanote/components/Layout/Layout.tsx
@@ -1,19 +1,26 @@
 import React, { ReactNode } from 'react'
+import Sidebar from '../Sidebar/Sidebar'
 
 function Layout({ children }: { children: ReactNode}) {
-  return (
-    <div className='flex flex-col items-center min-h-screen min-w-full bg-background max-h-screen'>
-        <nav>
-            nav
-            <div className='"flex gap-4 items-center'>
-                side
+return (
+    <div className='flex flex-col min-h-screen min-w-full bg-background max-h-screen'>
+ 
+            <div className="flex flex-row">
+                    <div className='p-[10px] flex gap-4 justify-center min-h-screen w-[200px]'>
+                            <Sidebar/>
+                    </div>
+
+                <div className='flex flex-col flex-grow'>
+                    <nav className='p-[10px] h-[50px]'>
+                        nav
+                    </nav>
+                    <main className='p-[10px] flex flex-w-full flex-grow' >
+                            {children}
+                    </main>
+                </div>
             </div>
-        </nav>
-        <main className='flex flex-w-full flex-grow'>
-            {children}
-        </main>
     </div>
-  )
+)
 }
 
 export default Layout
---
+++ neanote/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,21 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "../@/ui/card"
+
+function Sidebar() {
+  return (
+    <Card>
+
+        <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl">
+        Neanote
+        </h1>
+    </Card>
+  )
+}
+
+export default Sidebar
