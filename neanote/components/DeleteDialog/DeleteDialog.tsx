2ae7b9dd3eba0c13e2fdf368fb80e50c84c7ea34
nxtrm <nextrem_p@proton.me>
2024-08-03 19:41:38 +0300

    Improve archivecard ui

--- neanote/components/DeleteDialog/DeleteDialog.tsx
+++ neanote/components/DeleteDialog/DeleteDialog.tsx
@@ -2,8 +2,13 @@ import React, { ReactElement, ReactNode } from 'react'
 import { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogDescription, AlertDialogFooter, AlertDialogCancel, AlertDialogAction } from '../@/ui/alert-dialog'
 import { Button } from '../@/ui/button'
 
+interface Props {
+  children: ReactNode,
+  handleDelete: () => void,
+  handleArchive?: () => void
+}
 
-function DeleteDialog({children, handleDelete, handleArchive})  {
+function DeleteDialog({children, handleDelete, handleArchive}:Props)  {
   return (
     <AlertDialog>
         <AlertDialogTrigger asChild>
@@ -13,13 +18,15 @@ function DeleteDialog({children, handleDelete, handleArchive})  {
             <AlertDialogHeader>
             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
             <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete the note and all its components from the server. Maybe archive instead? 
+                This action cannot be undone. This will permanently delete the note and all its components from the server. {handleArchive && 'Maybe archive instead?' }
             </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
                 <div className='flex flex-row gap-3'>
                   <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  { handleArchive &&
                   <AlertDialogAction onClick={handleArchive}>Archive instead</AlertDialogAction>
+                  }
                   <AlertDialogAction onClick={handleDelete}>Delete</AlertDialogAction>
                 </div>
             </AlertDialogFooter>
--- neanote/src/Pages/Archive/Components/NoteCard.tsx
+++ neanote/src/Pages/Archive/Components/NoteCard.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import { ArchiveType } from '../../../api/types/archiveTypes'
+import { FaTasks } from 'react-icons/fa'
+import { MdRepeat } from 'react-icons/md'
+import { LuGoal } from 'react-icons/lu'
+import { Button } from '../../../../components/@/ui/button'
+import { MdOutlineRestore } from "react-icons/md";
+import { FaTrash } from 'react-icons/fa6'
+import DeleteDialog from '../../../../components/DeleteDialog/DeleteDialog'
+import { useArchive } from '../useArchive'
+
 
 function NoteCard({note}:{note:ArchiveType}) {
+    const {handleDelete, handleRestore} = useArchive();
   return (
     <div className='p-3 w-full rounded-xl border-[2px]'>
-        <div className='flex flex-row items-center gap-3 justify-between'>
-            <h3 className="task-title">{note.title}</h3>                                                       
+        <div className='flex flex-row justify-between '>
+            <div className='flex flex-row items-center gap-2' >
+                {note.type ==='task'&& <FaTasks size={'20px'}/>}
+                {note.type ==='habit'&& <MdRepeat size={'20px'}/>}
+                {note.type ==='goal'&& <LuGoal size={'22px'}/>}
+
+                <h3 className="task-title">{note.title}</h3>                                                       
+            </div>
+            <div className='flex flex-row items-center gap-2' >
+                <DeleteDialog handleDelete={handleDelete}>
+                    <Button size="icon" variant="destructive">
+                        <FaTrash />
+                    </Button>
+                </DeleteDialog>
+                
+                <Button size="icon">
+                    <MdOutlineRestore size={'20px'}/>
+                </Button>
+            </div>
             {/* {note.tags.map((tag, index) => (
               <TagLabel key={index} name={tag.name} color={tag.color} compressed={isTagCompressed}/>
             ))} */}
