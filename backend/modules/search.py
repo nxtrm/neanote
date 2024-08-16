162b774a6b7ab3d3f03ca474c490ccced9e54f82
nxtrm <nextrem_p@proton.me>
2024-08-16 14:40:07 +0300

    move searchbar funcitonality into a new file

---
+++ backend/modules/search.py
@@ -0,0 +1,121 @@
+from flask import g, jsonify, request
+from flask_jwt_extended import jwt_required
+import psycopg2
+
+from utils import token_required
+from word2vec import combine_strings_to_vector
+
+
+def search_routes(app, conn, model):
+    @app.route('/api/search', methods=['GET'])
+    @jwt_required()
+    @token_required
+    def search():
+        try:
+            user_id = g.userId
+            cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            search_query = request.args.get('searchQuery')
+            search_mode = request.args.get('searchMode')
+            page = int(request.args.get('pageParam', 1))
+            per_page = int(request.args.get('perPage', 5))
+            offset = (page - 1) * per_page
+
+            if search_mode == "approximate":
+                query_vector = combine_strings_to_vector([search_query], model, False)
+
+                cur.execute("""
+                    SELECT n.id AS note_id, n.title, n.content, n.type,
+                    t.id AS tagid,
+                    t.name,
+                    t.color,
+                    cosine_similarity(n.vector, %s) as similarity
+                    FROM Notes n
+                    LEFT JOIN NoteTags nt ON n.id = nt.note_id
+                    LEFT JOIN Tags t ON nt.tag_id = t.id
+                    WHERE n.user_id = %s AND n.archived = FALSE AND n.vector IS NOT NULL
+                    ORDER BY similarity DESC
+                    LIMIT %s OFFSET %s;
+                """, (query_vector, user_id, per_page, offset))
+
+            else:
+                cur.execute("""
+                    SELECT n.id AS note_id, n.title, n.content, n.type,
+                        t.id AS tagid,
+                        t.name,
+                        t.color
+                    FROM Notes n
+                        LEFT JOIN NoteTags nt ON n.id = nt.note_id
+                        LEFT JOIN Tags t ON nt.tag_id = t.id
+                    WHERE n.user_id = %s AND n.archived = FALSE
+                        AND (n.title ILIKE %s OR n.content ILIKE %s)
+                    LIMIT %s OFFSET %s;
+                """, (user_id, f"%{search_query}%", f"%{search_query}%", per_page, offset))
+
+            rows = cur.fetchall()
+
+            # Get the total number of results for pagination
+            if search_mode == "approximate":
+                cur.execute("""
+                    SELECT COUNT(*) FROM Notes n
+                    WHERE n.user_id = %s AND n.archived = FALSE AND n.vector IS NOT NULL;
+                """, (user_id,))
+            else:
+                cur.execute("""
+                    SELECT COUNT(*) FROM Notes n
+                    WHERE n.user_id = %s AND n.archived = FALSE
+                        AND (n.title ILIKE %s OR n.content ILIKE %s);
+                """, (user_id, f"%{search_query}%", f"%{search_query}%"))
+            
+            total = cur.fetchone()[0]
+            next_page = page + 1 if offset + per_page < total else None
+
+            notes_dict = {}
+            for row in rows:
+                note_id = row['note_id']
+                if note_id not in notes_dict:
+                    notes_dict[note_id] = {
+                        'noteid': note_id,
+                        'title': row['title'][:50] + '...' if len(row['title']) > 50 else row['title'],
+                        'content': row['content'][:100] + '...' if len(row['content']) > 100 else row['content'],
+                        'type': row['type'],
+                        'tags': []
+                    }
+                if row['tagid']:
+                    notes_dict[note_id]['tags'].append({
+                        'tagid': row['tagid'],
+                        'name': row['name'],
+                        'color': row['color']
+                    })
+
+            # Fetch the secondary ID based on the type
+            for note in notes_dict.values():
+                if note['type'] == 'task':
+                    cur.execute("SELECT id AS taskid FROM Tasks WHERE note_id = %s", (note['noteid'],))
+                    secondary_id = cur.fetchone()
+                    note['secondaryid'] = secondary_id['taskid'] if secondary_id else None
+                elif note['type'] == 'habit':
+                    cur.execute("SELECT id AS habitid FROM Habits WHERE note_id = %s", (note['noteid'],))
+                    secondary_id = cur.fetchone()
+                    note['secondaryid'] = secondary_id['habitid'] if secondary_id else None
+                elif note['type'] == 'goal':
+                    cur.execute("SELECT id AS goalid FROM Goals WHERE note_id = %s", (note['noteid'],))
+                    secondary_id = cur.fetchone()
+                    note['secondaryid'] = secondary_id['goalid'] if secondary_id else None
+                # Add any other types here
+
+            notes = list(notes_dict.values())
+
+            return jsonify({'message': 'Notes retrieved successfully', 'data': notes,
+                            'pagination': {
+                                    'total': total,
+                                    'page': page,
+                                    'perPage': per_page,
+                                    'nextPage': next_page
+                                }
+                            }), 200
+        except Exception as e:
+            conn.rollback()
+            print(f"An error occurred: {e}")
+            return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
+        finally:
+            cur.close()
